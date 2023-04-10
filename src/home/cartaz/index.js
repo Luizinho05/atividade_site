d454b94aaeb69b2843dca1a1c9efc16d6e1a2bcb
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-04-10 17:30:21 -0300

    atividade

--- src/home/cartaz/index.js
+++ src/home/cartaz/index.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react'
+
+import api from '../../services/api'
+
 export default function Cartaz(){
+
+    const [ filmes, setFilmes ] = useState([''])
+
+    useEffect(() => {
+        async function loadFilmes() {
+
+            const response = await api.get('/movie/now_playing', {
+                params: {
+                    api_key: "d7467b74ee29835a60176efafc99b899",
+                    language: 'pt-BR'
+                }
+            })
+
+            setFilmes(response.data.results)
+        }
+        loadFilmes()
+        console.log(filmes)
+    }, [filmes])
+
+    console.log(filmes)
+
     return(
         <div>
-           <h1>Cartaz</h1>
+           <h1> Em Cartaz</h1>
+            {filmes.map((filme) => {
+                return(
+                    <article>
+                        <h3>{filme.title}</h3>
+                    </article>
+                )
+            })}
         </div>
     )
 }
