88e9d81a8947f14a9bd63ca4b8d145ca3ec08e94
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-05-17 16:41:02 -0300

    atividade_site

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,14 +8,21 @@ export default function Header(){
         <div id="fundo">
   <Link className='logo' to='/'><img id="banner" src={banner} alt="banner"></img></Link>
   
-  <Link to='/informações'><h2 class="pagina">Info</h2></Link>
+
+<div class='dropdown'>
+  <button class='dropbtn'><h2 class='pagina'>Informações</h2></button>
+  <div class='dropdown-content'>
+  <Link class='textodropdown' to='/informações'>Info</Link>
+  </div>
+</div>
+
 
 <div class="dropdown">
   <button class="dropbtn"><h2 class="paginas">Filmes</h2></button>
   <div class="dropdown-content">
-  <Link to='/Filmes'>Populares</Link>
-  <Link to='/cartaz'>Em Cartaz</Link>
-  <Link to='/estreias'>Estreias</Link>
+  <Link class='textodropdown' to='/Filmes'>Populares</Link>
+  <Link class='textodropdown' to='/cartaz'>Em Cartaz</Link>
+  <Link class='textodropdown' to='/estreias'>Estreias</Link>
   </div>
 </div>
 
@@ -23,25 +30,25 @@ export default function Header(){
 <div class="dropdown">
   <button class="dropbtn"><h2 class="paginas">Favoritos</h2></button>
   <div class="dropdown-content">
-  <Link to='/Favoritos'>Filmes favoritos</Link>
-  <Link to='/Favorita'>Séries Favoritas</Link>
+  <Link class='textodropdown' to='/Favoritos'>Filmes favoritos</Link>
+  <Link class='textodropdown' to='/Favorita'>Séries Favoritas</Link>
   </div>
 </div>
 
 <div class="dropdown">
   <button class="dropbtn"><h2 class="paginas">Séries</h2></button>
   <div class="dropdown-content">
-  <Link to='/SerieP'>Populares</Link>
-  <Link to='/Hojetv'>Hoje na TV</Link>
-  <Link to='/tv'>Na TV</Link>
+  <Link class='textodropdown' to='/SerieP'>Populares</Link>
+  <Link class='textodropdown' to='/Hojetv'>Hoje na TV</Link>
+  <Link class='textodropdown' to='/tv'>Na TV</Link>
   </div>
 </div>
 
 <div class="dropdown">
-  <button class="dropbtn"><h2 class="paginas">Contato</h2></button>
+  <button class="dropbtn"><h2 class="paginas">Área Cliente</h2></button>
   <div class="dropdown-content">
-  <Link to='/Cadastro'>Cadastro</Link>
-  <Link to='/Login'>Login</Link>
+  <Link class='textodropdown' to='/Cadastro'>Cadastro</Link>
+  <Link class='textodropdown' to='/Login'>Login</Link>
   </div>
 </div>
 
